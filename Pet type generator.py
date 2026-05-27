28ab443eade0384a3b3525ecb8fe72b27e7292ed
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-27 17:50:57 +0200

    Third exercise

---
+++ Pet type generator.py
@@ -0,0 +1,14 @@
+name = "Kiara"
+animal_type = "cat"
+age = 11
+favorite_food = "wet food, chicken flavored"
+sleep_hours = 19
+awake_hours = 24 - sleep_hours
+
+print(f"Meet {name}!")
+print(f"She is a {animal_type} and she is {age} years old. She eats only {favorite_food}.")
+print(f"She is awake {awake_hours} hours per day.")
+
+coat_type = input("Does she have long hair? (True/False). ")
+
+print(f"You answered: {coat_type}, but she is still the most beautiful. ")
