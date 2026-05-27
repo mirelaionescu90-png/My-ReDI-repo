4d9921c14e8382ab4fbd61371e098a339438268f
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-27 16:49:11 +0200

    Extra practise

---
+++ New exercises.py
@@ -0,0 +1,20 @@
+name = input("What is your name? ")
+favorite_movie = input("What is your favorite movie? ")
+times_watched = int(input("How many times have you watched it? "))
+movie_rating = int(input("What rating would you give it (1-10)? "))
+print(f"Hi, {name}")
+print(f"My favorite movie is {favorite_movie} and I have watched it {times_watched}.")
+print(f"I am rating it {movie_rating}.")
+print("Wow, you really loved this movie!")
+
+
+name = "Mirela"
+this_year_destination = "Spain"
+trip_days = 4
+daily_budget = 30
+total_budget = trip_days * daily_budget
+travel_alone = input("Are you travelling alone, (yes/no)? ")
+print(f"Hi,{name}!")
+print(f"You are travelling this year to {this_year_destination}. ")
+print(f"Your daily budget is {daily_budget} and you will have in total {total_budget}€. ")
+print("You are so adventurous!")
---
+++ Week3 exercises and homework.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+age = 35
+city = "Copenhagen"
+hobby = "cycling"
+next_year = age + 1
+print(f"Hi! My name is {name}.")
+print(f"I am {age} years old and I live in {city}.")
+print(f"In my free time I love {hobby}.")
+print(f"Next year I will be {next_year} years old.")
+
