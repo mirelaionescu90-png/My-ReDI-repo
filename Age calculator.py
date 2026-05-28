ca3591dbd0a9139773a00ff307f015837bcab4f3
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-28 15:05:10 +0200

    Create Age calculator.py

---
+++ Age calculator.py
@@ -0,0 +1,7 @@
+birth_year = int(input("Enter your birth year: "))
+current_year = 2026
+
+my_age = 2026 - birth_year
+
+print(f"You are {my_age} years old. ")
+print(f"In five years, you will be {my_age + 5} years old. ")
