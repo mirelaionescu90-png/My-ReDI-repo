f1bb8e21850877387717ab3b7ee51bf7c2bb8eff
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-29 20:33:18 +0200

    Create Week5.py

---
+++ Week5.py
@@ -0,0 +1,18 @@
+raining = input("Is it raining? (yes/no) ")
+if raining == "yes":
+    print("Grab an umbrella!")
+
+else:
+    print("Enjoy the walk!")
+
+
+weather = input("What is the weather like? ")
+
+if weather == "storm":
+    print("Stay inside!")
+elif weather == "rain":
+    print("Grab an umbrella!")
+elif weather == "clouds":
+    print("Bring a jacket!")
+else:
+    print("Enjoy the sunshine!")
