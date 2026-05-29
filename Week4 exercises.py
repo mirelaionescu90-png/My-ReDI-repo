9bb2ab04eacfe8882e7990abc1ce9c2177769201
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-29 19:50:28 +0200

    Create Week4 exercises.py

---
+++ Week4 exercises.py
@@ -0,0 +1,45 @@
+x = 12
+
+if x > 10:
+    print("Big")
+else:
+    print("Small")
+
+
+age = 15
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+
+score = 88
+
+if score > 90:
+    print("A")
+elif score > 70:
+    print("B")
+else:
+    print("C")
+
+
+temperature = 0
+
+if temperature > 20:
+    print("Warm")
+elif temperature > 10:
+    print("Cool")
+else:
+    print("Cold")
+
+
+x = 5
+y = 5
+
+if x > y:
+    print("x")
+elif x == y:
+    print("equal")
+else:
+    print("y")
