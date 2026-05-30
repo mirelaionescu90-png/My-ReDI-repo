9dbc2a172456494e7c2a0f9f7764f948e90008a7
mirelaionescu90 <mirelaionescu90@gmail.com>
2026-05-30 12:06:38 +0200

    Create Week5-Loops.py

---
+++ Week5-Loops.py
@@ -0,0 +1,48 @@
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+
+for i in range(5):
+    print("Hello!")
+
+
+for i in range(10):
+    print(i)
+
+
+for i in range(0, 21):
+    print(i)
+
+
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
+
+
+for i in range(0, 21, 2):
+    print(i)
+
+
+count = 0
+while count < 21:
+    print(count)
+    count = count + 2
+
+
+names = ["Anna", "Bruno", "Carla"]
+
+for name in names:
+    print("Hello,", name)
+
+
+for letter in "Python":
+    print(letter)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print("I like", fruit)
